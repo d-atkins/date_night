889d03cc86a9b8aa204e07cc15e861a346450d45
David Atkins <atki1080@gmail.com>
2019-11-21 22:37:36 -0700

    Add test cases for first few insertions

--- test/binary_search_tree_test.rb
+++ test/binary_search_tree_test.rb
@@ -3,5 +3,29 @@ require 'minitest/pride'
 require './lib/binary_search_tree'
 
 class BinarySearchTreeTest < Minitest::Test
+  def test_it_exists
+    tree = BinarySearchTree.new
+
+    assert_instance_of BinarySearchTree, tree
+  end
+
+  def test_first_insert
+    tree = BinarySearchTree.new
+    tree.insert(61, "Bill & Ted's Excellent Adventure")
+
+    assert_instance_of Node, tree.root
+  end
+
+  #test node has title
+  #test node has score
+  #test nil children
+
+  def test_first_right_child
+    tree = BinarySearchTree.new
+    tree.insert(61, "Bill & Ted's Excellent Adventure")
+    tree.insert(92, "Sharknado 3")
+
+    assert_instance_of Node, tree.root.right_child
+  end
 
 end
