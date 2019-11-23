3b0a78c49bcdddcec4f2d2f37d04d6a5892c67c3
David Atkins <atki1080@gmail.com>
2019-11-23 14:08:34 -0700

    First step to refactoring to make code more reusable

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -5,62 +5,127 @@ class BinarySearchTree
   def initialize
   end
 
-  def insert(score, title)
-    node = Node.new(score, title)
+  def traverse(score)
     current_node = @root
-    depth = 0
-    if !@root
-      @root = node
-    else
-      depth += 1
-      loop do
-        if node.score > current_node.score
-          if current_node.right_child
-            current_node = current_node.right_child
-            depth += 1
-          else
-            current_node.set_right(node)
-            break
-          end
-        elsif node.score < current_node.score
-          if current_node.left_child
-            current_node = current_node.left_child
-            depth += 1
-          else
-            current_node.set_left(node)
-            break
-          end
+
+    #traverse the tree, return the node whose
+    #score is equal to parameter, or the last node found
+    #return nil if no nodes found
+    loop do
+      if current_node.nil?
+        return current_node
+
+      elsif score > current_node.score
+        if current_node.right_child
+          current_node = current_node.right_child
         else
-          break
+          return current_node
+        end
+
+      elsif score < current_node.score
+        if current_node.left_child
+          current_node = current_node.left_child
+        else
+          return current_node
         end
       end
+
     end
-    return depth
+
+  end
+
+  def insert(score, title)
+    #create new node -> node = Node.new(score, title)
+    #invoke traverse(score) -> store in t_node
+        #do nothing if include?(score)
+    #set node to root if traverse returns nil
+    #set node to t_node left child if score is less
+    #set node to t_node right child if score is greater
+
+    if include?(score)
+      return
+    end
+
+    node = Node.new(score, title)
+    parent_node = traverse(score)
+
+    if !parent_node
+      @root = node
+    elsif node.score > parent_node.score
+      parent_node.set_right(node)
+    elsif node.score < parent_node.score
+      parent_node.set_left(node)
+    end
+
+    # node = Node.new(score, title)
+    # current_node = @root
+    # depth = 0
+    # if !@root
+    #   @root = node
+    # else
+    #   depth += 1
+    #   loop do
+    #     if node.score > current_node.score
+    #       if current_node.right_child
+    #         current_node = current_node.right_child
+    #         depth += 1
+    #       else
+    #         current_node.set_right(node)
+    #         break
+    #       end
+    #     elsif node.score < current_node.score
+    #       if current_node.left_child
+    #         current_node = current_node.left_child
+    #         depth += 1
+    #       else
+    #         current_node.set_left(node)
+    #         break
+    #       end
+    #     else
+    #       break
+    #     end
+    #   end
+    # end
+    # return depth
   end
 
   def include?(score)
-    current_node = @root
+    #invoke traverse on score -> store in t_node
+    #return true if t_node.score = score
+    #otherwise return false
 
-    loop do
-      if current_node.score == score
-        return true
-      else
-
-        if score < current_node.score
-          if current_node.left_child
-            current_node = current_node.left_child
-          else
-            return false
-          end
-        elsif score > current_node.score
-          if current_node.right_child
-            current_node = current_node.right_child
-          else
-            return false
-          end
-        end
-      end
+    node = traverse(score)
+    if node.nil? || node.score != score
+      false
+    elsif node.score == score
+      true
     end
+
+    # current_node = @root
+    #
+    # loop do
+    #   if current_node.score == score
+    #     return true
+    #   else
+    #     if score < current_node.score
+    #       if current_node.left_child
+    #         current_node = current_node.left_child
+    #       else
+    #         return false
+    #       end
+    #     elsif score > current_node.score
+    #       if current_node.right_child
+    #         current_node = current_node.right_child
+    #       else
+    #         return false
+    #       end
+    #     end
+    #   end
+    # end
+  end
+
+  def depth_of(score)
+
   end
 
 end
